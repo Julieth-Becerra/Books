11f63d030a584d28371ecbc37d49a869260df1f7
Julieth-Becerra <julieth.becerra01@uptc.edu.co>
2021-11-10 11:56:29 -0500

    actualizacion

--- main.cpp
+++ main.cpp
@@ -8,44 +8,52 @@ int main() {
     LinkedList<string>* linkFirst = new LinkedList<string>();
     LinkedList<string>* linkLast = new LinkedList<string>();
 
-    //D-C-B-A
 
-    linkFirst->addNodeFirst("Bahduhaosd");
-    //linkFirst->addNodeFirst('C');
-    //linkFirst->addNodeFirst('D');
-
-    //A-B-C-D
-    linkLast->addNodeLast("");
-    linkLast->addNodeLast('B');
-    linkLast->addNodeLast('C');
-    linkLast->addNodeLast('D');
 
     char opc = 0;
-    cout <<"1. Agregar al inicio \n"<<"2. Agregar al final \n"<<"3. Buscar nodo \n"<<"4. Buscar libro \n"<<"5.Insetar antes nodo\n"<<"6.Insertar despues nodo";
-    cout<<opc;
-    switch (opc != 7) {
+        cout <<"1. Agregar al inicio \n"<<"2. Agregar al final \n"<<"3. Buscar nodo \n"<<"4. Buscar libro \n"<<"5.Insetar antes nodo\n"<<"6.Insertar despues nodo";
+        cin>>opc;
+        switch (opc != 7)
+        {
 
-        case 1:
-            const std::string libro = "";
-            cout<<"Digite libro a agregar";
-            cout<<libro;
+            case 1: {
+                std::string libro = "";
+                cout << "Digite libro a agregar";
+                cin >> libro;
 
-            linkFirst->addNodeFirst(libro);
+                linkFirst->addNodeFirst(libro);
+                for (string val: linkFirst->getLinkedList()) {
+                    cout << val << "-";
+                }
+                cout << endl;
+            }
+                break;
+
+            case 2: {
+                std::string libro1 = "";
+                cout << "Digite libro a agregar";
+                cin >> libro1;
+
+                linkLast->addNodeLast(libro1);
+
+                for (string val: linkLast->getLinkedList()) {
+                    cout << val << "-";
+                }
+
+                cout << endl;
+            }
+                break;
+            case 3:
+            {
+
+            }
             break;
-        case 2:
+        }
 
-    }
-    for( string val : linkFirst->getLinkedList() ){
-        cout<<val<<"-";
-    }
 
-    cout<<endl;
 
-    for( char val : linkLast->getLinkedList() ){
-        cout<<val<<"-";
-    }
 
-    cout<<endl;
+
 
     delete(linkFirst);
 
